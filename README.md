f7e7014ff6ee7f1cfc7f83fffcb2cb9d08bd52a9
andresvcc <43545168+andresvcc@users.noreply.github.com>
2019-05-25 16:48:12 +0200

    d

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # projBD
+
+pour suivre les logs du serveur 
+
+1) login heroku dans la console(terminal) avec le comand : heroku login 
+
+
+heroku logs --source app --tail
--- src/components/list.jsx
+++ src/components/list.jsx
@@ -6,47 +6,14 @@ export default class List extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: [],
-            message: [],
-            page: null,
-        }
+            users: []
 
-        this.clickGet = this.clickGet.bind(this)
-        this.clickPost = this.clickPost.bind(this)
+        }
     }
 
     componentWillMount() {
         request
-            .post('http://localhost:3000/films')
-            .end((err, res) => {
-                const users = JSON.parse(res.text).results;
-                this.setState({
-                    users: users
-                });
-            });
-    }
-
-    clickGet(e) {
-        request
-            .get('http://localhost:3000/test')
-            .end((err, res) => {
-                const msg = JSON.parse(res.text).msg;
-                this.setState({
-                    message: msg
-                });
-            });
-    }
-
-    clickPost(e) { 
-        request
-            .post('http://localhost:3000/addFilm')
-            .send({ nom: 'Manny11'}) // sends a JSON post body
-            .end((err, res) => {
-                err ? console.log(err) : console.log(res)
-            });
-            
-        request
-            .post('http://localhost:3000/films')
+            .post('https://projbd.herokuapp.com/listFilm')
             .end((err, res) => {
                 const users = JSON.parse(res.text).results;
                 this.setState({
@@ -58,12 +25,10 @@ export default class List extends Component {
     render() {
         return (
             <div>
-                <button onClick={this.clickGet}>Get</button>
-                <button onClick={this.clickPost}>Post</button>
-                <p>------</p>
+
                 <Noms values={this.state.users}></Noms>
-                <p>------</p>
-                {this.state.message.toString()}
+
+
             </div>
         )
     }
